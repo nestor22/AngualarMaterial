1ae57291d41b71719776b6dea3fb0b45b7ebd50e
Nestor Gerardo Gonzalez Roque <nestor.ge222@gmail.com>
2021-07-27 22:03:10 -0600

    lazy load dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch:'full' },
   {path: 'login', component: LoginComponent},
+  {path: 'dashboard', loadChildren: ()=>import('./components/dashboard/dashboard.module').then(x=>x.DashboardModule) },
   {path: '**', redirectTo: 'login'}
 
 ];

381b835f183dd1d916c4c05ba40c6e1726d96b9d
Nestor Gerardo Gonzalez Roque <nestor.ge222@gmail.com>
2021-08-19 10:59:05 -0600

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { MateriadashComponent } from './materiadash/materiadash.component';
+import { OternativationComponent } from './oternativation/oternativation.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch:'full' },
-  {path: 'login', component: LoginComponent},
-  {path: 'dashboard', loadChildren: ()=>import('./components/dashboard/dashboard.module').then(x=>x.DashboardModule) },
-  {path: '**', redirectTo: 'login'}
-
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashmateria', component: MateriadashComponent },
+  { path: 'otromenu', component: OternativationComponent },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./components/dashboard/dashboard.module').then(
+        (x) => x.DashboardModule
+      ),
+  },
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,40 @@ import { SharedModule } from './shared/shared.module';
 
 
 //components
-import { LoginComponent } from './components/login/login.component'
+import { LoginComponent } from './components/login/login.component';
+import { MateriadashComponent } from './materiadash/materiadash.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { OternativationComponent } from './oternativation/oternativation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MateriadashComponent,
+    OternativationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
 
   ],
   providers: [],
